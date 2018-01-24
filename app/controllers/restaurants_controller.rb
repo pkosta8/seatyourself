1a460f2539521003b43275ecb712795f479a2e52
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-24 15:38:22 -0500

    controller time and index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,22 +13,16 @@ class RestaurantsController < ApplicationController
 @restaurant.summary                 = params[:restaurant][:summary]
 @restaurant.price                   = params[:restaurant][:price]
 @restaurant.owner_id                = params[:restaurant][:owner_id]
-
-# @restaurant.open_time = Time.utc(params[:restaurant]["open_time(1i)"].to_i,
-#                                       params[:restaurant]["open_time(2i)"].to_i,
-#                                       params[:restaurant]["open_time(3i)"].to_i,
 #
+# @restaurant.open_time               =  params[:restaurant]["open_time(1i)"].to_i,
 #
-# @restaurant.close_time = Time.utc(params[:restaurant]["close_time(1i)"].to_i,
-#                                         params[:restaurant]["close_time(2i)"].to_i,
-#                                         params[:restaurant]["close_time(3i)"].to_i,
-#
+# @restaurant.close_time              =  params[:restaurant]["close_time(1i)"].to_i,
 #
 # @restaurant.user_id = current_user.id
 #
-#
 
-    if @restaurant.save
+
+    if restaurant.save
       redirect_to restaurants_url
 
     else
