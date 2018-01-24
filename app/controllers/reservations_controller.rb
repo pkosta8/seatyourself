589e8a16410a29c2507eb5d939025dc5136ab55c
vioif <ireneaflores3@gmail.com>
2018-01-24 12:24:21 -0500

    reservation controller made nothing in reservations view yet

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,52 @@
 class ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservations.all
+  end
+
+  def show
+    @resevation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+    @resevation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservaiton = Reservation.new
+
+    @reservaiton.name             = params[:reservation][:name]
+    @reservaiton.date             = params[:reservation][:date]
+    @reservaiton.number_of_people = params[:reservation][:number_of_people]
+
+      if @resevation.save
+        redirect_to resturants_path, notice: 'Resevations have succesfully been booked'
+      else
+        render 'resturants'
+      end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    @reservaiton.name             = params[:reservation][:name]
+    @reservaiton.date             = params[:reservation][:date]
+    @reservaiton.number_of_people = params[:reservation][:number_of_people]
+
+    if @resevation.save
+      redirect_to resturants_path, notice: 'Resevations have succesfully been updated '
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reservaiton = Reservation.find(params[:id])
+    @reservaiton.destory
+    redirect_to "/resturants"
+  end
 end
