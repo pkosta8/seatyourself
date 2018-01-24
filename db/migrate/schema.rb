707e541bc6760bb2c1ed3e1dc76c01995a6874bb
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-24 13:42:12 -0500

    restaurant controller, viewform_restaurants, modelrestaurants migraterestaurants

--- db/migrate/schema.rb
+++ db/migrate/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123222244) do
+ActiveRecord::Schema.define(version: 20180124160751) do
+
+  create_table "restaurants", force: :cascade do |t|
+
+      t.string   "name"
+      t.integer  "capacity"
+      t.string   "address"
+      t.string   "menu"
+      t.string  "summary"
+      t.string   "price"
+      t.datetime "open_time"
+      t.datetime "close_time"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
