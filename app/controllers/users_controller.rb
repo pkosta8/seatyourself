ec0c91719b81da6e5895a776fc9061a04901502b
vioif <ireneaflores3@gmail.com>
2018-01-25 11:22:25 -0500

    added users index and fixed up resevationd controller removed S in reservations index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def new
     @user = User.new
   end
@@ -16,9 +19,15 @@ class UsersController < ApplicationController
     p @user
 
   if @user.save
-    redirect_to users_url
+    redirect_to users_path
   else
     render :new
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
 end
